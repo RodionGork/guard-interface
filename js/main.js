49b22d0dc39578c34eca92a83c7821ca2ebb5e07
Rodion Gorkovenko <RodionGork@github.com>
2016-08-04 09:03:54 +0300

    Fixed positioning, added weather and time

--- js/main.js
+++ js/main.js
@@ -4,14 +4,33 @@ $(function() {
     
     $(window).resize(setupScale);
     
+    setInterval(timeUpdate, 300);
+    
     function setupScale() {
-        var w = $('#main').width() + 50;
-        var h = $('#main').height() + 30;
+        var w = $('body').width();
+        var h = $('body').height();
         var cw = $(window).width();
         var ch = $(window).height();
         var ratio = Math.min(cw / w, ch / h);
-        console.log(ratio);
-        $('#main').css('transform', 'scale(' + ratio + ')');
+        var dx = Math.round((cw - w * ratio) / 2);
+        var dy = Math.round((ch - h * ratio) / 2);
+        console.log(ratio + ', ' + dx + ', ' + dy);
+        var s = 'matrix(' + ratio + ', 0, 0, ' + ratio + ', ' + dx +',' + dy + ')';
+        $('body').css('transform', s);
+    }
+    
+    function twoDigits(n) {
+        var s = '00' + n;
+        return s.substring(s.length - 2);
+    }
+    
+    function timeUpdate() {
+        var m = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля',
+                'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+        var d = new Date();
+        var date = d.getDate() + ' ' + m[d.getMonth()] + ' ' + d.getFullYear();
+        var time = twoDigits(d.getHours()) + ':' + twoDigits(d.getMinutes()) + ':' + twoDigits(d.getSeconds());
+        $('.time').html(date + '<br/>' + time);
     }
     
 });
